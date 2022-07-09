33ad90cbf3c72e24160c03e53434cc1f649b4ce6
Ali Fahs <ali.fahs@activeeon.com>
2022-07-10 01:51:56 +0200

    stupid mistake 2

--- kube-scripts/install-kube.sh
+++ kube-scripts/install-kube.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # This bash script is designed to prepare and install docker and Kubernetes for Ubuntu 18.04
 # The stpes were taken from https://phoenixnap.com/kb/install-kubernetes-on-ubuntu with modifications done once necessary.
 # Author: Ali Jawad FAHS, Activeeon
@@ -41,24 +40,28 @@ log_print INFO "Logs are saved at: $LOGFILE"
 log_print INFO "Updating the package list."
 sudo apt-get update
 
+# Install curl
+log_print INFO "Installing curl"
+sudo apt-get install -y curl || { log_print ERROR "curl installation failed!"; exit 1; }
+
 # Install Docker
 log_print INFO "Installing Docker"
-sudo apt-get install docker.io
+sudo apt-get install -y docker.io
 sudo systemctl enable docker
 sudo systemctl status docker
 sudo systemctl start docker
 
-sudo docker -v || log_print ERROR "Docker installation failed!"; exit 1
+sudo docker -v || { log_print ERROR "Docker installation failed!"; exit 1; }
 
 
 # Install Kubernetes
 log_print INFO "Installing Kubernetes"
 
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" || log_print ERROR "Kubernetes repo can't be added!"; exit 1
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" || { log_print ERROR "Kubernetes repo can't be added!"; exit 1; }
 
-sudo apt-get install kubeadm  || log_print ERROR "kubeadm installation failed!"; exit 1
-sudo apt-get install kubectl  || log_print ERROR "kubectl installation failed!"; exit 1
-sudo apt-get install kubelet  || log_print ERROR "kubelet installation failed!"; exit 1
+sudo apt-get install -y kubeadm  || { log_print ERROR "kubeadm installation failed!"; exit 1; }
+sudo apt-get install -y kubectl  || { log_print ERROR "kubectl installation failed!"; exit 1; }
+sudo apt-get install -y kubelet  || { log_print ERROR "kubelet installation failed!"; exit 1; }
 
 
 # Hoding upgrades for Kubernetes software (versions to updated manually)
@@ -67,9 +70,9 @@ sudo apt-mark hold kubeadm kubelet kubectl
 # Checking for the installiation versions
 log_print INFO "Checking Kubernetes versions"
 
-kubeadm version 	|| log_print ERROR "Docker installation failed!"; exit 1
-kubectl version     || log_print ERROR "Docker installation failed!"; exit 1
-kubelet --version   || log_print ERROR "Docker installation failed!"; exit 1
+kubeadm version     || { log_print ERROR "Docker installation failed!"; exit 1; }
+kubectl version     || { log_print ERROR "Docker installation failed!"; exit 1; }
+kubelet --version   || { log_print ERROR "Docker installation failed!"; exit 1; }
 
 
 # Turn off the swap momery
@@ -77,11 +80,12 @@ if [ `grep Swap /proc/meminfo | grep SwapTotal: | cut -d" " -f14` == "0" ];
 	then
 		log_print INFO "The swap memory is Off"
 	else
-		sudo swapoff –a || log_print ERROR "swap memory can't be turned off "; exit 1;
+		sudo swapoff –a || { log_print ERROR "swap memory can't be turned off "; exit 1; }
 	fi
 
 
 # Declare configuration done successfully
 ENDTIME=$(date +%s)
 ELAPSED=$(( ENDTIME - STARTTIME ))
-log_print INFO "Configuration done successfully in $ELAPSED seconds"
+log_print INFO "Configuration done successfully in $ELAPSED seconds "
+
